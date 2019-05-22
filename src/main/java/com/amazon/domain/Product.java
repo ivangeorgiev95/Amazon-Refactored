05a4799d6d8b578282623ecda4110524bc1ee53f
Ivan_Georgiev <ivan.georgiev95@yahoo.com>
2019-05-22 17:56:40 +0300

    setup

---
+++ src/main/java/com/amazon/domain/Product.java
@@ -0,0 +1,23 @@
+package com.amazon.domain;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private Integer quantity;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+//    private Seller seller;
+}
