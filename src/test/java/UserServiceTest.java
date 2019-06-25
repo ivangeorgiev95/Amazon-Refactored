7fcdd3d739d9cc73123f05e855ff72b0e4574431
Ivan_Georgiev <ivan.georgiev95@yahoo.com>
2019-06-25 17:27:27 +0300

    user service test 2

--- src/test/java/UserServiceTest.java
+++ src/test/java/UserServiceTest.java
@@ -1,5 +1,11 @@
+import com.amazon.domain.Address;
+import com.amazon.domain.CreditCard;
+import com.amazon.domain.ShoppingCart;
 import com.amazon.domain.User;
+import com.amazon.dto.LoginDTO;
 import com.amazon.dto.UserRegistrationDTO;
+import com.amazon.exceptions.InvalidEmailException;
+import com.amazon.exceptions.InvalidPasswordException;
 import com.amazon.exceptions.UserException;
 import com.amazon.repository.*;
 import com.amazon.service.UserService;
@@ -10,6 +16,11 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import static org.mockito.Mockito.*;
@@ -17,6 +28,12 @@ import static org.mockito.Mockito.*;
 @RunWith(SpringRunner.class)
 public class UserServiceTest {
 
+    private static final String TEST_EMAIL = "test@amazon.com";
+    private static final String TEST_PASSWORD = "123456";
+    private static final String TEST_NAME = "Test";
+    private static final String INVALID_PASSWORD = "654321";
+    public static final long TEST_ID = 1L;
+    public static final String TEST_CREDIT_CARD_NUMBER = "4523182363972080";
     @Mock
     private UserRepository userRepository;
     @Mock
@@ -36,34 +53,80 @@ public class UserServiceTest {
     @InjectMocks
     private UserService userService;
 
-    private UserRegistrationDTO registrationDTO;
-
+    private UserRegistrationDTO testRegistrationForm;
+    private LoginDTO testLoginForm;
+    private User testUser;
+    private String hashedPassword = SensitiveDataEncryption.hashSensitiveData(TEST_PASSWORD);
     @Before
     public void init(){
-        registrationDTO = new UserRegistrationDTO("Test", "test@amazon.com","123456","123456");
+        testRegistrationForm = new UserRegistrationDTO(TEST_NAME, TEST_EMAIL, TEST_PASSWORD, TEST_PASSWORD);
+        testLoginForm = new LoginDTO(TEST_EMAIL, TEST_PASSWORD);
+        testUser = new User();
+        testUser.setPassword(hashedPassword);
     }
 
     @Test
     public void registerBusyEmail(){
-        when(userRepository.findByEmail("test@amazon.com")).thenReturn(new User());
-        assertThrows(UserException.class, () -> userService.registerUser(registrationDTO));
+        when(userRepository.findByEmail(TEST_EMAIL)).thenReturn(new User());
+        assertThrows(UserException.class, () -> userService.registerUser(testRegistrationForm));
     }
 
     @Test
     public void registerNewUser() throws UserException {
-        User testUser = new User();
-        testUser.setId(1L);
+        testUser.setId(TEST_ID);
         when(userRepository.save(any(User.class))).thenReturn(testUser);
-        assertEquals((long) 1, (long) userService.registerUser(registrationDTO));
+        assertEquals(TEST_ID, (long) userService.registerUser(testRegistrationForm));
     }
 
     @Test
-    public void loginTest(){
-        String hashedPassword = SensitiveDataEncryption.hashSensitiveData(registrationDTO.getPassword());
-        User testUser = new User();
-        testUser.setPassword(hashedPassword);
-        when(userRepository.findByEmail(registrationDTO.getEmail())).thenReturn(testUser);
-//        assertEquals(testUser, userService.logUser());
+    public void setNewShoppingCartWhenRegisterTest() throws UserException {
+        when(userRepository.save(any(User.class))).thenReturn(testUser);
+        when(shoppingCartRepository.save(any(ShoppingCart.class))).thenReturn(new ShoppingCart());
+        userService.registerUser(testRegistrationForm);
+        assertNotNull(testUser.getShoppingCart());
+    }
+
+    @Test
+    public void successfulLoginTest() throws InvalidEmailException, InvalidPasswordException {
+        when(userRepository.findByEmail(testLoginForm.getEmail())).thenReturn(testUser);
+        assertEquals(testUser, userService.logUser(testLoginForm));
+    }
+
+    @Test
+    public void loginWithInvalidEmailTest(){
+        when(userRepository.findByEmail(testLoginForm.getEmail())).thenReturn(null);
+        assertThrows(InvalidEmailException.class, () -> userService.logUser(testLoginForm));
+    }
+
+    @Test
+    public void loginWithInvalidPasswordTest(){
+        testLoginForm.setPassword(INVALID_PASSWORD);
+        when(userRepository.findByEmail(testLoginForm.getEmail())).thenReturn(testUser);
+        assertThrows(InvalidPasswordException.class, () -> userService.logUser(testLoginForm));
+    }
+
+    @Test
+    public void addNewAddressTest(){
+        Address testAddress = new Address();
+        testAddress.setId(TEST_ID);
+        testUser.setAddresses(new ArrayList<Address>());
+        when(addressRepository.save(any(Address.class))).thenReturn(testAddress);
+        assertTrue(userService.addNewAddress(new Address(), testUser) == TEST_ID && testUser.getAddresses().contains(testAddress));
+    }
+
+    @Test
+    public void addCreditCardWhichUserAlreadyContains(){
+        CreditCard existingCreditCard = new CreditCard();
+        String hashedCreditCardNumber = SensitiveDataEncryption.hashSensitiveData(TEST_CREDIT_CARD_NUMBER);
+        existingCreditCard.setCardNumber(hashedCreditCardNumber);
+        testUser.setCreditCards(new HashSet<CreditCard>(Arrays.asList(existingCreditCard)));
+        CreditCard testCreditCard = new CreditCard();
+        testCreditCard.setCardNumber(TEST_CREDIT_CARD_NUMBER);
+        assertThrows(UserException.class, () -> userService.addNewCreditCard(testUser,testCreditCard));
+    }
+
+    @Test
+    public void addNonExistingCreditCard(){
 
     }
 
